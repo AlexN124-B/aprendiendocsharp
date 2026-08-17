e1b372157c0e92f06ebb2cb1a9cf17d520f71b61
unknown <an658716@gmail.com>
2026-08-17 15:28:23 +0200

    añadiendo iterable, su modelo persona y su iterador en la vista principal

---
+++ proyecto de pruebas/Models/Persona.cs
@@ -0,0 +1,13 @@
+﻿namespace proyecto_de_pruebas.Models;
+
+public class Persona
+{
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+
+    public Persona(string nombre, int edad)
+    {
+        Nombre = nombre;
+        Edad = edad;
+    }
+}
--- proyecto de pruebas/ViewModels/MainViewModel.cs
+++ proyecto de pruebas/ViewModels/MainViewModel.cs
@@ -1,4 +1,6 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Collections.Generic;
+using proyecto_de_pruebas.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace proyecto_de_pruebas.ViewModels;
 
@@ -11,4 +13,13 @@ public partial class MainViewModel : ViewModelBase
 
     [ObservableProperty]
     public partial string colorFondo { get; set; } = "white";
+
+    public List<Persona> Personas { get; set; } = 
+    [
+        new Persona("Juan", 20),
+        new Persona("María", 25),
+        new Persona("Pedro", 20),
+        new Persona("Laura", 22)
+    ];
+
 }
